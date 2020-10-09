48cc93dce2b229bde226cc0b21138948e3020077
Niko Escobal <niko.escobal@gmail.com>
2020-10-09 19:09:43 +0800

    Fix rubocop errors.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,40 +1,38 @@
+# frozen_string_literal: true
+
 # bubble_sort
-# def bubble_sort(arr)
-#   i = 0
-#   while i < arr.length
-#     x = i + 1
-#     while x < arr.length
-#       arr[x], arr[i] = arr[i], arr[x] if arr[x] < arr[i]
-#       x += 1
-#     end
-#     i += 1
-#   end
-#   arr
-# end
-# y = bubble_sort([1, 3, 5, 7])
-# puts y
+def bubble_sort(arr)
+  i = 0
+  while i < arr.length
+    x = i + 1
+    while x < arr.length
+      arr[x], arr[i] = arr[i], arr[x] if arr[x] < arr[i]
+      x += 1
+    end
+    i += 1
+  end
+  arr
+end
+y = bubble_sort([1, 3, 5, 7])
+puts y
 
 # bubble_sort_by
 def bubble_sort_by(arr)
-  i=0
-  
+  i = 0
+
   while i < arr.length
     x = i + 1
-   for y in x..(arr.length-1)  do
-    
-     c = yield(arr[i],arr[y])
-     
-      if c.positive?
-      arr[y],arr[i]=arr[i],arr[y]
-      
-      end
-   end
+    (x..(arr.length - 1)).each do |y|
+      c = yield(arr[i], arr[y])
+
+      arr[y], arr[i] = arr[i], arr[y] if c.positive?
+    end
     i += 1
   end
   arr
 end
 
- y=bubble_sort_by(["hi","hello","hey"]) do |left,right|
-  left.length-right.length
+y = bubble_sort_by(%w[hi greetings hello salutations hey]) do |left, right|
+  left.length - right.length
 end
 print y
