4c917322dfa92a7565cb8890704eb362bbb337e6
jamestucker1988 <xat2212@gmail.com>
2020-10-09 16:31:27 +0530

    fix bubble_sort_by program logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,34 +1,40 @@
 # bubble_sort
-def bubble_sort(arr)
-  i = 0
-  while i < arr.length
-    x = i + 1
-    while x < arr.length
-      arr[x], arr[i] = arr[i], arr[x] if arr[x] < arr[i]
-      x += 1
-    end
-    i += 1
-  end
-  arr
-end
-y = bubble_sort([1, 3, 5, 7])
-puts y
+# def bubble_sort(arr)
+#   i = 0
+#   while i < arr.length
+#     x = i + 1
+#     while x < arr.length
+#       arr[x], arr[i] = arr[i], arr[x] if arr[x] < arr[i]
+#       x += 1
+#     end
+#     i += 1
+#   end
+#   arr
+# end
+# y = bubble_sort([1, 3, 5, 7])
+# puts y
 
 # bubble_sort_by
 def bubble_sort_by(arr)
-  yield arr
-  arr
-end
-y = bubble_sort_by([1, 7, 5, 3]) do |arr|
-  i = 0
+  i=0
+  
   while i < arr.length
     x = i + 1
-    while x < arr.length
-      arr[x], arr[i] = arr[i], arr[x] if arr[x] < arr[i]
-      x += 1
-    end
+   for y in x..(arr.length-1)  do
+    
+     c = yield(arr[i],arr[y])
+     
+      if c.positive?
+      arr[y],arr[i]=arr[i],arr[y]
+      
+      end
+   end
     i += 1
   end
+  arr
 end
-# y = bubble_sort_by()
-puts y
+
+ y=bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length-right.length
+end
+print y
